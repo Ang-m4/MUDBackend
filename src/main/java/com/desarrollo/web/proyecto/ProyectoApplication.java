5569f929da381ea99a2ac40d3e1376a1ad6419ac
andres giraldo <giraldoma@javeriana.edu.co>
2022-03-05 00:46:39 -0500

    Logger Added

--- src/main/java/com/desarrollo/web/proyecto/ProyectoApplication.java
+++ src/main/java/com/desarrollo/web/proyecto/ProyectoApplication.java
@@ -1,15 +1,19 @@
 package com.desarrollo.web.proyecto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class ProyectoApplication {
 
+	Logger log = LoggerFactory.getLogger(getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
-
 	}
-	
 
 }
