9f735793bc227203e898c1cf4b91acb0fde29595
Andres <giraldoma@javeriana.edu.co>
2022-03-04 22:29:21 -0500

    Creacion del proyecto

---
+++ src/test/java/com/desarrollo/web/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.web.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
