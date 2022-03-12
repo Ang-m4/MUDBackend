31b6367f87c9ccbe9514290c68988f62ef8c173f
Ang-m4 <giraldoma@javeriana.edu.co>
2022-03-11 20:50:35 -0500

    NPCController renamed

--- src/main/java/com/desarrollo/web/proyecto/Controllers/MonsterController.java
+++ src/main/java/com/desarrollo/web/proyecto/Controllers/MonsterController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/monster")
-public class NPCController {
+public class MonsterController {
 
     Logger log = LoggerFactory.getLogger(getClass());
 
